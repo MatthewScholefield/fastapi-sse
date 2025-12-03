2187aba3b262ca2cb7880a7f5b9b94d83d57e273
nlsg <93901799+nlsg@users.noreply.github.com>
2025-12-03 09:54:29 +0100

    implement test for optional fields being excluded (#4)
    Co-authored-by: nils <n.kappeler@bioengineering.ch>
    Co-authored-by: Matthew D. Scholefield <matthew331199@gmail.com>

--- test_fastapi_sse.py
+++ test_fastapi_sse.py
@@ -15,6 +15,11 @@ class MyEvent(BaseModel):
     message: str
 
 
+class MyEventWithOptionals(BaseModel):
+    message: str
+    metadata: str | None
+
+
 async def emit_my_events() -> AsyncGenerator[MyEvent, Any]:
     for i in range(3):
         yield MyEvent(message=f'Test message {i}')
@@ -29,6 +34,16 @@ async def my_events_handler():
         yield MyEvent(message=f'Test message {i}')
 
 
+@app.get('/test-stream-containing-optionals')
+@sse_handler()
+async def my_events_with_optionals_handler():
+    for i in range(3):
+        await asyncio.sleep(0.1)
+        yield MyEventWithOptionals(
+            message=f'Test message {i}', metadata='sometimes' if i == 0 else None
+        )
+
+
 @app.get('/test-stream-sequence')
 @sse_handler()
 async def my_events_handler_sequence():
@@ -36,15 +51,13 @@ async def my_events_handler_sequence():
         await asyncio.sleep(0.1)
         yield [MyEvent(message=f'Test message {i}-{j}') for j in range(3)]
 
+
 @app.get('/test-stream-typed-sequence')
 @typed_sse_handler()
 async def my_events_handler_typed_sequence():
     for i in range(3):
         await asyncio.sleep(0.1)
         yield [MyEvent(message=f'Test message {i}-{j}') for j in range(3)]
-
-
-
 
 
 @app.get('/test-stream-typed')
@@ -88,6 +101,17 @@ async def test_sse_sequence_response():
         assert events[2] == dict(data='[{"message":"Test message 2-0"},{"message":"Test message 2-1"},{"message":"Test message 2-2"}]')
 
 
+@pytest.mark.asyncio
+async def test_sse_response_containing_optionals():
+    async with TestClient(app) as client:
+        response = await client.get('/test-stream-containing-optionals', stream=True)
+        events = await collect_events_data(response)
+
+        assert response.status_code == 200
+        assert events[0] == dict(data='{"message":"Test message 0","metadata":"sometimes"}')
+        assert events[1] == dict(data='{"message":"Test message 1"}')
+        assert events[2] == dict(data='{"message":"Test message 2"}')
+
 
 @pytest.mark.asyncio
 async def test_typed_sse_response():
@@ -100,6 +124,7 @@ async def test_typed_sse_response():
         assert events[1] == dict(data='{"message":"Test message 1"}', event='MyEvent')
         assert events[2] == dict(data='{"message":"Test message 2"}', event='MyEvent')
 
+
 @pytest.mark.asyncio
 async def test_typed_sequence_sse_response():
     async with TestClient(app) as client:
@@ -108,8 +133,6 @@ async def test_typed_sequence_sse_response():
 
         assert response.status_code == 200
         assert events[0]["event"] == "MyEvent"
-
-
 
 
 @pytest.mark.asyncio
